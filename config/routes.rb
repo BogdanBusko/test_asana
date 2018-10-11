0a07ecee912a542206d0420bb4d210e7cbb32b12
Busko Bogdan <busko.bogdan@gmail.com>
2018-10-11 16:54:48 +0300

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: "dashboard#index"
+
+  namespace :account do
+    resources :projects, only: [:index, :show, :create]
+  end
 end
