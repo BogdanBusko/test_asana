4482965d3e68af62d54122d65c21c34279b61c2e
Busko Bogdan <busko.bogdan@gmail.com>
2018-10-11 17:09:04 +0300

    prerender: true

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -2,5 +2,5 @@ ReactOnRails.configure do |config|
   config.build_production_command = "RAILS_ENV=production NODE_ENV=production bin/webpack"
   config.build_test_command = "RAILS_ENV=test bin/webpack"
   config.server_bundle_js_file = "index.js"
-  # config.prerender = true
+  config.prerender = true
 end
